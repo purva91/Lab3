2958c186fbee2aee84bc08b16a8f9073ed53e135
Tanmaya <tanela16@gmail.com>
2014-02-19 22:44:19 -0700

    Lab3_fullquery test

--- Lab3/src/Lab3Task1.java
+++ Lab3/src/Lab3Task1.java
@@ -7,175 +7,191 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 /**
-
+ * 
  * Servlet implementation class Lab3task1
-
  */
 
 @WebServlet("/Lab3Task1")
+public class Lab3Task1 extends HttpServlet {
 
-public class Lab3Task1 extends HttpServlet 
-{
-	
-	 File file  = new File("/Users/Cynosure/Downloads/ResultEntries.txt");
+	File file = new File("/Users/Cynosure/Downloads/ResultEntries.txt");
 	private static final long serialVersionUID = 1L;
-    /**
 
-     * @see HttpServlet#HttpServlet()
-
-     */
-
-    public Lab3Task1() 
-    {
-    	// TODO Auto-generated constructor stub
-    	super();
-    }
 	/**
-	
-	* @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	
-	*/
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
-	{
-		//Map<String,String[]> queryParams = request.getParameterMap();
-		response.setContentType("text/html");
-	    PrintWriter out = response.getWriter();
-		Enumeration<String> params = request.getParameterNames();
-		BufferedReader read = new BufferedReader(new FileReader(file.getAbsoluteFile()));
-	      
-    	String fileEntry=read.readLine();
-    		while(fileEntry!=null)
-    		{
-    			HashMap<String,String> fileMap = new HashMap<String,String>();
-    			String[] fileValues = fileEntry.split(",");
-    			/*TODO split the file entry and store in the fileHashMap in corresponding values*/
-    			fileMap.put("firstname", fileValues[0]);
-    			fileMap.put("lastname", fileValues[1]);
-    			fileMap.put("dob ",fileValues[2]);
-    			fileMap.put("languages", fileValues[3]);
-    			fileMap.put("days", fileValues[4]);
-    			
-    			/*TODO: pass the keys of query parameter map to file HashMap to check if the value
-    			 *  of fileHashMap contains the same value or substring of value .
-    			 * */
-    			
-    			
-    			while(params.hasMoreElements())
-    			{
-    				/*Get the value of each parameter, pass the parameter as the key in fileHasMap */
-    				String paramN = (String)params.nextElement();
-    		    	  String[] paramV =
-    		    			  request.getParameterValues(paramN);
-    		    			  String paramValue = paramV[0];
-    		    	String checkValue = fileMap.get(paramN);
-    		    	if(checkValue.contains(paramValue)){
-    		    		out.println("<html>"+checkValue+"</html>");break;}
-    			}
-    			break;
-    			//fileEntry=read.readLine();
-    		}
-    		
-    		read.close();
+	 * 
+	 * @see HttpServlet#HttpServlet()
+	 */
+
+	public Lab3Task1() {
+		// TODO Auto-generated constructor stub
+		super();
 	}
-	
+
 	/**
-	
-	* @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	
-	*/
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
-	{
+	 * 
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		// Map<String,String[]> queryParams = request.getParameterMap();
+		boolean set = false;
+		ArrayList<String> result = new ArrayList<String>();
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		Enumeration<String> params = request.getParameterNames();
+		BufferedReader read = new BufferedReader(new FileReader(
+				file.getAbsoluteFile()));
+
+		String fileEntry = read.readLine();
+		String saveEntry = fileEntry;
+		while (fileEntry != null) {
+			HashMap<String, String> fileMap = new HashMap<String, String>();
+			String[] fileValues = fileEntry.split(",");
+			/*
+			 * TODO split the file entry and store in the fileHashMap in
+			 * corresponding values
+			 */
+			fileMap.put("firstname", fileValues[0]);
+			fileMap.put("lastname", fileValues[1]);
+			fileMap.put("dob ", fileValues[2]);
+			fileMap.put("languages", fileValues[3]);
+			fileMap.put("days", fileValues[4]);
+
+			/*
+			 * TODO: pass the keys of query parameter map to file HashMap to
+			 * check if the value of fileHashMap contains the same value or
+			 * substring of value .
+			 */
+
+			while (params.hasMoreElements()) {
+				/*
+				 * Get the value of each parameter, pass the parameter as the
+				 * key in fileHasMap
+				 */
+				String paramN = (String) params.nextElement();
+				String[] paramV = request.getParameterValues(paramN);
+				if (paramV.length == 1) {
+					String paramValue = paramV[0];
+					String checkValue = fileMap.get(paramN);
+					if (checkValue.contains(paramValue))
+						set = true;
+					else {
+						set = false;
+						break;
+					}
+				}
+
+				else {
+					for (int i = 0; i < paramV.length; i++) {
+						String paramValue = paramV[i];
+						String checkValue = fileMap.get(paramN);
+						if (checkValue.contains(paramValue))
+							set = true;
+					}
+				}
+
+			}
+			if (set==true)
+				result.add(saveEntry);
+			fileEntry = read.readLine();
+			saveEntry = fileEntry;
+		}
+
+		read.close();
+		if (result.isEmpty()) {
+			for (int i = 0; i < result.size(); i++) {
+				out.println("<html>\"" + result.get(i) + "</html>");
+			}
+		}
+	}
+
+	/**
+	 * 
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		//doGet(request, response);
-		int cntEntries=0;
+		// doGet(request, response);
+		int cntEntries = 0;
 		String fileEntry = "";
 		response.setContentType("text/html");
 		PrintWriter writeToFile;
-	      PrintWriter out = response.getWriter();
-	      
-	     Enumeration<String> params = request.getParameterNames();
-	      
-	     
-	      while(params.hasMoreElements())
-	      {
-	    	  String paramN = (String)params.nextElement();
-	    	  String[] paramV =
-	    			  request.getParameterValues(paramN);
-	    			  if (paramV.length == 1) {
-	    			  String paramValue = paramV[0];
-	    			  if (paramValue.length() == 0)
-	    			  fileEntry = fileEntry+"no value"+",";
-	    			  else
-	    			  fileEntry = fileEntry+paramValue+",";
-	    			  }
-	    			  
-	    			  else {
-	    			  for(int i=0; i<paramV.length; i++) 
-	    			  {
-	    				  
-	    				  if (paramV[i].length() == 0)
-	    	    			  fileEntry = fileEntry+"no value"+" ";
-	    	    			  else
-	    	    			  fileEntry = fileEntry+paramV[i]+" ";
-	    			  }
-	    			  }
-	    			 
-	      }
-	      
-	      if(!file.exists())
-	      {
-	    	  file.createNewFile();
-	    	 writeToFile = new PrintWriter(file); 
-	    	 writeToFile.write(fileEntry);
-	    	 writeToFile.append("\n");
-	      }
-	      
-	      else {
-	    	   writeToFile = new PrintWriter(new FileWriter(file,true)); 
-	      writeToFile.append(fileEntry);
-	      writeToFile.append("\n");
-	      }
-	      
-	      writeToFile.close();
-		fileEntry="";
-	    			  
-	    	BufferedReader read = new BufferedReader(new FileReader(file.getAbsoluteFile()));
-	      
-	    	fileEntry=read.readLine();
-	    		while(fileEntry!=null)
-	    		{
-	    			++cntEntries;
-	    			fileEntry=read.readLine();
-	    		}
-	    		
-	    		read.close();
-	    		
-	    		
-	    			  
-	   out.println("<html>"+"<body>Transaction successful<br>Number of entries are:"+cntEntries+"<br><a href=\".."+request.getContextPath()+"/Form1.html\">click here to go back</a></body></html>");
-	      
-		 /* String title = "Using GET Method to Read Form Data Changed";
-	      String docType =
-	      "<!doctype html public \"-//w3c//dtd html 4.0 " +
-	      "transitional//en\">\n";
-	      out.println(docType +
-	                "<html>\n" +
-	                "<head><title>" + title + "</title></head>\n" +
-	                "<body bgcolor=\"#f0f0f0\">\n" +
-	                "<h1 align=\"center\">" + title + "</h1>\n" +
-	                "<ul>\n" +
-	                "  <li><b>First Name</b>: "
-	                + request.getParameter("firstname") + "\n" +
-	                "  <li><b>Last Name</b>: "
-	                + request.getParameter("lastname") + "\n" +
-	                "</ul>\n" +
-	                "</body></html>");*/
+		PrintWriter out = response.getWriter();
 
-		
-		
+		Enumeration<String> params = request.getParameterNames();
+
+		while (params.hasMoreElements()) {
+			String paramN = (String) params.nextElement();
+			String[] paramV = request.getParameterValues(paramN);
+			if (paramV.length == 1) {
+				String paramValue = paramV[0];
+				if (paramValue.length() == 0)
+					fileEntry = fileEntry + "no value" + ",";
+				else
+					fileEntry = fileEntry + paramValue + ",";
+			}
+
+			else {
+				for (int i = 0; i < paramV.length; i++) {
+
+					if (paramV[i].length() == 0)
+						fileEntry = fileEntry + "no value" + " ";
+					else
+						fileEntry = fileEntry + paramV[i] + " ";
+				}
+			}
+
+		}
+
+		if (!file.exists()) {
+			file.createNewFile();
+			writeToFile = new PrintWriter(file);
+			writeToFile.write(fileEntry);
+			writeToFile.append("\n");
+		}
+
+		else {
+			writeToFile = new PrintWriter(new FileWriter(file, true));
+			writeToFile.append(fileEntry);
+			writeToFile.append("\n");
+		}
+
+		writeToFile.close();
+		fileEntry = "";
+
+		BufferedReader read = new BufferedReader(new FileReader(
+				file.getAbsoluteFile()));
+
+		fileEntry = read.readLine();
+		while (fileEntry != null) {
+			++cntEntries;
+			fileEntry = read.readLine();
+		}
+
+		read.close();
+
+		out.println("<html>"
+				+ "<body>Transaction successful<br>Number of entries are:"
+				+ cntEntries + "<br><a href=\".." + request.getContextPath()
+				+ "/Form1.html\">click here to go back</a></body></html>");
+
+		/*
+		 * String title = "Using GET Method to Read Form Data Changed"; String
+		 * docType = "<!doctype html public \"-//w3c//dtd html 4.0 " +
+		 * "transitional//en\">\n"; out.println(docType + "<html>\n" +
+		 * "<head><title>" + title + "</title></head>\n" +
+		 * "<body bgcolor=\"#f0f0f0\">\n" + "<h1 align=\"center\">" + title +
+		 * "</h1>\n" + "<ul>\n" + "  <li><b>First Name</b>: " +
+		 * request.getParameter("firstname") + "\n" + "  <li><b>Last Name</b>: "
+		 * + request.getParameter("lastname") + "\n" + "</ul>\n" +
+		 * "</body></html>");
+		 */
+
 	}
 }
